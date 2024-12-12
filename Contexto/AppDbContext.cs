7cddb6497ad1302d58122b0760392b37e3194211
DavidCT <DavidCuervoTunon@gmail.com>
2024-12-12 10:20:53 +0100

    Generado el tipo de dato de los campos decimal en las facturas de venta

--- Contexto/AppDbContext.cs
+++ Contexto/AppDbContext.cs
@@ -9,6 +9,25 @@ namespace AppSincronizadorERP.Contexto
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<FacturaVentaCabecera>(entity =>
+            {
+                entity.Property(e => e.BaseImponible).HasColumnType("decimal(18,2)");
+                entity.Property(e => e.ImpIVA).HasColumnType("decimal(18,2)");
+                entity.Property(e => e.ImpTotal).HasColumnType("decimal(18,2)");
+            });
+
+            modelBuilder.Entity<FacturaVentaLineas>(entity =>
+            {
+                entity.Property(e => e.Cantidad).HasColumnType("decimal(18,2)");
+                entity.Property(e => e.Precio).HasColumnType("decimal(18,2)");
+                entity.Property(e => e.Importe).HasColumnType("decimal(18,2)");
+            });
+        }
+
         public DbSet<Clientes> Clientes { get; set; }
         public DbSet<FacturaVentaCabecera> FacturaVentaCabecera { get; set; }
         public DbSet<FacturaVentaLineas> FacturaVentaLineas { get; set; }
