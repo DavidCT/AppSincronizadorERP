977484b55ec8e54d28dd70f7fa23b7f0d5b292c9
David <DavidCuervoTunon@gmail.com>
2024-12-13 09:34:56 +0100

    Se han cambiado la definicion de los datos decimales en la propia entidad

--- Contexto/AppDbContext.cs
+++ Contexto/AppDbContext.cs
@@ -5,28 +5,7 @@ namespace AppSincronizadorERP.Contexto
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<FacturaVentaCabecera>(entity =>
-            {
-                entity.Property(e => e.BaseImponible).HasColumnType("decimal(18,2)");
-                entity.Property(e => e.ImpIVA).HasColumnType("decimal(18,2)");
-                entity.Property(e => e.ImpTotal).HasColumnType("decimal(18,2)");
-            });
-
-            modelBuilder.Entity<FacturaVentaLineas>(entity =>
-            {
-                entity.Property(e => e.Cantidad).HasColumnType("decimal(18,2)");
-                entity.Property(e => e.Precio).HasColumnType("decimal(18,2)");
-                entity.Property(e => e.Importe).HasColumnType("decimal(18,2)");
-            });
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Clientes> Clientes { get; set; }
         public DbSet<FacturaVentaCabecera> FacturaVentaCabecera { get; set; }
--- Entidades/FacturaVentaCabecera.cs
+++ Entidades/FacturaVentaCabecera.cs
@@ -6,14 +6,20 @@ namespace AppSincronizadorERP.Entidades
     [Table("tbFacturaVentaCabecera")]
     public class FacturaVentaCabecera
     {
-        [Key] public required int IDFactura {  get; set; }
+        [Key] public required int IDFactura { get; set; }
         public required string NFactura { get; set; }
         public required DateTime FechaFactura { get; set; }
         public required string IDCliente { get; set; }
-        public string ? RazonSocial { get; set; }
-        public string ? CifCliente { get; set; }
+        public string? RazonSocial { get; set; }
+        public string? CifCliente { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal BaseImponible { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal ImpIVA { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal ImpTotal { get; set; }
     }
 }
