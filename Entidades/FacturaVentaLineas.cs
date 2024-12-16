e6f6f5104f3a52c151745376da82549aca074441
DavidCT <DavidCuervoTunon@gmail.com>
2024-12-16 14:56:40 +0100

    Cambio en las propiedades de las líneas de las facturas

--- Entidades/FacturaVentaLineas.cs
+++ Entidades/FacturaVentaLineas.cs
@@ -10,8 +10,14 @@ namespace AppSincronizadorERP.Entidades
         public required int IDFactura { get; set; }
         public required string IDArticulo { get; set; }
         public string? DescArticulo { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Cantidad { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Precio { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Importe { get; set; }
         public DateTime? FechaCreacionAudi { get; set; }
     }
