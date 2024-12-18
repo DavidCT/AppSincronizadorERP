7a1d9ec8c2b1c8acabaf9026c469caaf95359d3c
DavidCT <DavidCuervoTunon@gmail.com>
2024-12-18 13:44:53 +0100

    Añadidos campos nuevos a la entidad de artículos

--- Entidades/Articulos.cs
+++ Entidades/Articulos.cs
@@ -9,5 +9,9 @@ namespace AppSincronizadorERP.Entidades
         [Key] public required string IDArticulo { get; set; }
         public required string DescArticulo { get; set; }
         public DateTime? FechaAlta { get; set; }
+        public string? IDEstado { get; set; }
+        public string? IDTipo { get; set; }
+        public string? IDFamilia { get; set; }
+        public string? IDSubFamilia { get; set; }
     }
 }
